cb7115df185fbb0bcca6cf60d3de5852709e59ce
Aehmlo Lxaitn <aehmlo@aehmlo.com>
2015-08-29 18:07:06 -0500

    Add subrproject to version control.

---
+++ prefs/BGLRootListController.m
@@ -0,0 +1,13 @@
+#include "BGLRootListController.h"
+
+@implementation BGLRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
+	}
+
+	return _specifiers;
+}
+
+@end
