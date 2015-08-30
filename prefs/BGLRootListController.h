0f24abe24394066cfce6e29e3684568d3a99b9fb
Aehmlo Lxaitn <aehmlo@aehmlo.com>
2015-08-30 15:52:45 -0500

    Use Cephei in prefs.

--- prefs/BGLRootListController.h
+++ prefs/BGLRootListController.h
@@ -1,5 +1,14 @@
 #import <Preferences/PSListController.h>
+#import <Cephei/HBListController.h>
 
-@interface BGLRootListController : PSListController
+@interface BGLRootListController : HBListController
+
++ (NSString *)hb_specifierPlist {
+	return @"Root";
+}
+
++ (UIColor *)hb_tintColor {
+	return [UIColor blueColor];
+}
 
 @end
